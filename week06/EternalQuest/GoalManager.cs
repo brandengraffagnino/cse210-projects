f99fc0cb3c316892c15014f0af1977f232c252e6
brandengraffagnino <brandengraffagnino@gmail.com>
2026-06-12 16:08:16 -0700

    finished the stubs for the eternal quest project

---
+++ week06/EternalQuest/GoalManager.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+
+public class GoalManager
+{
+    private List<Goal> _goals;
+    private int _score;
+
+    public GoalManager()
+    {
+        _goals = new List<Goal>();
+        _score = 0;
+    }
+
+    public void Start()
+    {
+    }
+
+    public void DisplayPlayerInfo()
+    {
+    }
+
+    public void CreateGoal()
+    {
+    }
+
+    public void RecordEvent()
+    {
+    }
+
+    public void ShowGoals()
+    {
+    }
+}
---
+++ week06/EternalQuest/SimpleGoal.cs
@@ -0,0 +1,16 @@
+public class SimpleGoal : Goal
+{
+    private bool _isComplete;
+
+    public SimpleGoal(string name, string description, int points)
+        : base(name, description, points)
+    {
+        _isComplete = false;
+    }
+
+    public override int RecordEvent()
+    {
+        _isComplete = true;
+        return _points;
+    }
+}
