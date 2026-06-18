defdd91275e6aea9234533aba8308517c64464c8
brandengraffagnino <brandengraffagnino@gmail.com>
2026-06-18 16:53:43 -0700

    finished assignment

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        activities.Add(new Running("17 June 2026", 30, 3.0));
+        activities.Add(new Cycling("17 June 2026", 30, 6.0));
+        activities.Add(new Swimming("17 June 2026", 30, 20));
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
